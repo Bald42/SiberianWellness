bdd7fe4adcbd77b8ec1adc1e8ead8492d39f5e4f
Andru@DESKTOP-MCIB5DC <bald.andrushin@gmail.com>
2020-01-09 23:05:11 +0700

    Начал писать основные скрипты. Создал структуру папок и проекта. Встроил EditorHelper. аписал применение цветов.

---
+++ Assets/GameResources/Scripts/Spawns/SpawnColorButtons.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Создаём кнопки цветов
+/// </summary>
+public class SpawnColorButtons : MonoBehaviour
+{
+    [SerializeField]
+    private ColorsInfo colorsInfo = null;
+
+    [SerializeField]
+    private GameObject prefabButton = null;
+
+    [SerializeField]
+    private Transform parentObject = null;
+
+    private const string KEY_CLONE = "(Clone)";
+
+    private void Awake()
+    {
+        Spawn();
+    }
+
+    /// <summary>
+    /// Создаём кнопки
+    /// </summary>
+    private void Spawn ()
+    {
+        if (colorsInfo && colorsInfo.Colors.Count > 0)
+        {
+            for (int i=0; i < colorsInfo.Colors.Count; i++)
+            {
+                GameObject newButton = Instantiate(prefabButton,parentObject);
+                newButton.name = newButton.name.Replace(KEY_CLONE, "_" + i.ToString());
+                newButton.GetComponent<ButtonColor>().Init(colorsInfo.Colors[i]);
+            }
+        }
+    }
+}
